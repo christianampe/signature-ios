469c7dbd77dd28cd3821851266dc0c46d2b8645e
Christian Ampe <christianampe@icloud.com>
2019-09-29 21:40:13 -0700

    Initial Commit

---
+++ SignatureView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignatureView
+//
+//  Created by Christian Ampe on 9/29/19.
+//  Copyright © 2019 Christian Ampe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
